d4f36dc33c8b1684be61b7ebc75e3bdda9c38ee0
Faizovr <triknf@gmail.com>
2020-10-12 18:40:27 +0300

    Init cw

---
+++ src/corewar/init_data.c
@@ -0,0 +1,10 @@
+#include "corewar.h"
+#include "libft.h"
+
+void init_data(int argc, char **argv, t_data *data)
+{
+	data->a_flag = 0;
+	data->d_flag = 0;
+	data->n_flags = 0;
+	data->cars_amount = 0;
+}
---
+++ src/corewar/main.c
@@ -0,0 +1,14 @@
+#include "corewar.h"
+#include "libft.h"
+#include "utils.h"
+
+int main(int ac, char *av[])
+{
+	t_data data;
+
+	if (ac == 1)
+		exit_with_error(ARGS_MSG, E_NONE);
+
+
+	return (0);
+}
